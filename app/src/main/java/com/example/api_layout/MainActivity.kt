2842ec99756a2d3de21bd26a96bec66a527dea21
simon-hash <Buddha8919>
2020-11-29 13:48:18 +0100

    changes 1

--- app/src/main/java/com/example/api_layout/MainActivity.kt
+++ app/src/main/java/com/example/api_layout/MainActivity.kt
@@ -1,17 +1,35 @@
 package com.example.api_layout
 
 import android.content.Intent
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
 import kotlinx.android.synthetic.main.activity_main.*
+import java.util.*
+
 
 class MainActivity : AppCompatActivity() {
+
+    val foodList = arrayListOf(
+            "Indisches Curry", "Burger", "Maultaschen",
+            "Spaghetti", "Minestrone", "Steak"
+    )
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+
+        decide.setOnClickListener {
+            val random = Random()
+            val randomFood = random.nextInt(foodList.count())
+            Foodid.text = foodList[randomFood]
+        }
+
         totutorial.setOnClickListener {
-            val intent = Intent(this,SecondActivity::class.java)
+            val cont = Foodid.text
+            val intent = Intent(this, SecondActivity::class.java)
+            intent.putExtra(SecondActivity.FOOD_Name,cont)
             startActivity(intent)
         }
 
--- app/src/main/java/com/example/api_layout/SecondActivity.kt
+++ app/src/main/java/com/example/api_layout/SecondActivity.kt
@@ -1,40 +1,63 @@
 package com.example.api_layout
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
 import com.google.android.youtube.player.YouTubeInitializationResult
 import com.google.android.youtube.player.YouTubePlayer
 import com.google.android.youtube.player.YouTubePlayerSupportFragment
+import kotlinx.android.synthetic.main.activity_main.*
+
 
 class SecondActivity : AppCompatActivity() {
-    override fun onCreate(savedInstanceState: Bundle?) {
+
+    companion object {
+        const val FOOD_Name = "food_passer"
+    }
+
+
+    override fun onCreate(savedInstanceState: Bundle?){
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_second)
+
+        initUI()
+    }
+
+    private fun initUI() {
+
+        val foodmap: Map<String, String> = mapOf(
+                "Indisches Curry" to "9DQ5QaAsCDs",
+                "Burger" to "vCXg4tuMpe4",
+                "Maultaschen" to "xA5P75X_xGE",
+                "Spaghetti" to "Io3osssFJ6o",
+                "Minestrone" to "Usp-Rgyn2kc",
+                "Steak" to "mdl6CPSifiI"
+        )
+        val vidId = foodmap[intent.getStringExtra(FOOD_Name)]
+
 
         var youtubefragment = supportFragmentManager.findFragmentById(R.id.youtube_fragment)
                 as YouTubePlayerSupportFragment
 
-        youtubefragment.initialize("AIzaSyBo2cWBK3_eOdYiKpcNyfp6P4TE4B3nUEw",object: YouTubePlayer.OnInitializedListener{
+        youtubefragment.initialize("AIzaSyBo2cWBK3_eOdYiKpcNyfp6P4TE4B3nUEw", object : YouTubePlayer.OnInitializedListener {
             override fun onInitializationSuccess(provider: YouTubePlayer.Provider?, player: YouTubePlayer?, wasRestored: Boolean) {
-                if(player == null) return
-                if(wasRestored)
+                if (player == null) return
+                if (wasRestored)
                     player.play()
-                else{
-                    player.cueVideo("Flxi_47SvD0")
+                else {
+                    player.cueVideo(vidId)
                     player.setPlayerStyle(YouTubePlayer.PlayerStyle.DEFAULT)
                 }
 
             }
 
             override fun onInitializationFailure(
-                p0: YouTubePlayer.Provider?,
-                p1: YouTubeInitializationResult?
+                    p0: YouTubePlayer.Provider?,
+                    p1: YouTubeInitializationResult?
             ) {
 
             }
 
         })
-
-
     }
+
 }
